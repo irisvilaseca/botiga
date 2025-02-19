11771b8ef21837c01ae07d498290d627b835d030
Brandy <2023_brandy.sherile@iticbcn.cat>
2025-02-19 20:51:20 +0100

    cart and orders models added

--- cart/models.py
+++ cart/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 
-from catalog.models import Product
+from botiga.catalog.models import Product
 
 
 class Cart(models.Model):
